4985bfdceaedd144c225bfd2c7a5b5dc8376d216
Thanh <name123091@gmail.com>
2025-02-09 10:46:38 +0700

    init home page and abouttUs page

--- src/pages/about-us/index.tsx
+++ src/pages/about-us/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import AboutUs from 'src/components/about-us'
+import AboutUs from 'src/components/about-us/AboutUs'
+
 
 export default function index() {
   return (
     <div>
-      <AboutUs />
+      <AboutUs/>
     </div>
   )
 }

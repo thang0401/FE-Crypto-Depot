88795794ed6918c02c2a44ce760f0a3ace606ef9
prok13332 <157262761+prok13332@users.noreply.github.com>
2025-02-24 20:27:08 +0700

    Update import src of SavingsPortfolio index.tsx

--- src/pages/saving/add-saving-asset/open/index.tsx
+++ src/pages/saving/add-saving-asset/open/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Deposit from 'src/components/make-transaction/deposit-asset'
-import SavingsPortfolioForm from 'src/components/saving/open/1SavingsPortfolioForm'
+import SavingsPortfolioForm from 'src/components/saving/open/SavingsPortfolioForm'
 export default function index() {
   return (
     <div>

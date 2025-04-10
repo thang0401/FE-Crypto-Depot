a6342d5271bc8d9d694a171d5b085abf0e4de2c9
thang0401 <khongbietthang0@gmail.com>
2025-04-10 09:32:15 +0700

    initialize-wallet

--- src/context/types.ts
+++ src/context/types.ts
@@ -1,26 +1,32 @@
 export type ErrCallbackType = (err: { [key: string]: string }) => void
 
 export type LoginParams = {
-  email: string
+  username: string
   password: string
   rememberMe?: boolean
 }
 
 export type UserDataType = {
-  id: number
+  id: string
   role: string
   email: string
   fullName: string
   username: string
   password: string
   avatar?: string | null
+  kycStatus : boolean
+  walletAddress? : string | null
+  firstName : string
 }
 
+
 export type AuthValuesType = {
-  loading: boolean
-  logout: () => void
-  user: UserDataType | null
-  setLoading: (value: boolean) => void
-  setUser: (value: UserDataType | null) => void
-  login: (params: LoginParams, errorCallback?: ErrCallbackType) => void
-}
+  loading: boolean;
+  logout: () => void;
+  user: UserDataType | null;
+  setLoading: (value: boolean) => void;
+  setUser: (value: UserDataType | null) => void;
+  login: (params: LoginParams, errorCallback?: ErrCallbackType) => void;
+  otpRequired: boolean; // Thêm
+  handleOtpVerify: (otp: string, errorCallback?: ErrCallbackType) => void; // Thêm
+};

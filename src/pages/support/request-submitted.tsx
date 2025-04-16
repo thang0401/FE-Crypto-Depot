62515ae7da4e01407852fe32b9533278fae06647
Thanh <name123091@gmail.com>
2025-04-16 11:40:38 +0700

    init support/claim

---
+++ src/pages/support/request-submitted.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import RequestSubmitted from 'src/components/support/request-submitted'
+
+const index = () => {
+  return (
+    <RequestSubmitted/>
+  )
+}
+
+export default index

269601bc70db8a03a8e977a4ca3f0b42637ca793
thang0401 <khongbietthang0@gmail.com>
2025-04-29 21:34:55 +0700

    add UI deposit/ withdraw from web3 wallet

---
+++ src/components/make-transaction/deposit-asset/type.ts
@@ -0,0 +1,20 @@
+export interface User {
+  id: string
+  name: string
+  phone: string
+  avatar?: string
+  lastTransaction?: string
+}
+
+export interface TransactionDetails {
+  amount: string
+  fee: string
+  note: string
+  passCode: string
+}
+
+export interface FormErrors {
+  amount: boolean
+  passCode: boolean
+}
+

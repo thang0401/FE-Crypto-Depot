0276805edf09d7fc58b8062a5b08043ed4d4e775
thang0401 <khongbietthang0@gmail.com>
2025-05-05 09:15:21 +0700

    internal transaction

--- src/components/make-transaction/transfer-debit/recipient-search.tsx
+++ src/components/make-transaction/transfer-debit/recipient-search.tsx
@@ -1,109 +1,109 @@
-import type React from "react"
-import { CardContent, Typography, List, ListItemAvatar, ListItemText, Avatar, Box, InputAdornment, Dialog, DialogTitle,
- DialogContent,
- DialogActions,
- Button,} from "@mui/material"
-import SearchIcon from "@mui/icons-material/Search"
-import HistoryIcon from "@mui/icons-material/History"
-import { StyledCard, SearchBar, RecentUserItem } from "./styled-components"
-import { User } from "./type"
+// import type React from "react"
+// import { CardContent, Typography, List, ListItemAvatar, ListItemText, Avatar, Box, InputAdornment, Dialog, DialogTitle,
+//  DialogContent,
+//  DialogActions,
+//  Button,} from "@mui/material"
+// import SearchIcon from "@mui/icons-material/Search"
+// import HistoryIcon from "@mui/icons-material/History"
+// import { StyledCard, SearchBar, RecentUserItem } from "./styled-components"
+// import { User } from "./type"
 
-interface DialogState {
-  open: boolean
-  message: string
-}
+// interface DialogState {
+//   open: boolean
+//   message: string
+// }
 
-interface RecipientSearchProps {
-  phoneFilter: string
-  selectedUser: User | null
-  recentUsers: User[] // Chỉ hiển thị 5 người gần đây
-  onPhoneFilterChange: (e: React.ChangeEvent<HTMLInputElement>) => void
-  onUserSelect: (user: User) => void
-  dialog: DialogState
-  onCloseDialog: () => void
-}
+// interface RecipientSearchProps {
+//   phoneFilter: string
+//   selectedUser: User | null
+//   recentUsers: User[] // Chỉ hiển thị 5 người gần đây
+//   onPhoneFilterChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+//   onUserSelect: (user: User) => void
+//   dialog: DialogState
+//   onCloseDialog: () => void
+// }
 
-export default function RecipientSearch({
-  phoneFilter,
-  selectedUser,
-  recentUsers,
-  onPhoneFilterChange,
-  onUserSelect,
-  dialog,
-  onCloseDialog,
-}: RecipientSearchProps) {
-  return (
-    <StyledCard>
-      <CardContent>
-        <Typography variant="h6" gutterBottom>
-          Tìm người nhận
-        </Typography>
+// export default function RecipientSearch({
+//   phoneFilter,
+//   selectedUser,
+//   recentUsers,
+//   onPhoneFilterChange,
+//   onUserSelect,
+//   dialog,
+//   onCloseDialog,
+// }: RecipientSearchProps) {
+//   return (
+//     <StyledCard>
+//       <CardContent>
+//         <Typography variant="h6" gutterBottom>
+//           Tìm người nhận
+//         </Typography>
 
-        <SearchBar
-          fullWidth
-          placeholder="Nhập số điện thoại của người nhận"
-          value={phoneFilter}
-          onChange={onPhoneFilterChange}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <SearchIcon />
-              </InputAdornment>
-            ),
-          }}
-          helperText="Nhập số điện thoại 10 chữ số đầy đủ để tìm kiếm"
-        />
+//         <SearchBar
+//           fullWidth
+//           placeholder="Nhập số điện thoại của người nhận"
+//           value={phoneFilter}
+//           onChange={onPhoneFilterChange}
+//           InputProps={{
+//             startAdornment: (
+//               <InputAdornment position="start">
+//                 <SearchIcon />
+//               </InputAdornment>
+//             ),
+//           }}
+//           helperText="Nhập số điện thoại 10 chữ số đầy đủ để tìm kiếm"
+//         />
 
-        <Box sx={{ mt: 2 }}>
-          <Typography variant="subtitle1" sx={{ display: "flex", alignItems: "center", mb: 1 }}>
-            <HistoryIcon fontSize="small" sx={{ mr: 1 }} />
-            Giao dịch gần đây
-          </Typography>
+//         <Box sx={{ mt: 2 }}>
+//           <Typography variant="subtitle1" sx={{ display: "flex", alignItems: "center", mb: 1 }}>
+//             <HistoryIcon fontSize="small" sx={{ mr: 1 }} />
+//             Giao dịch gần đây
+//           </Typography>
 
-          <List sx={{ py: 0 }}>
-            {recentUsers.map((user) => (
-              <RecentUserItem
-                key={user.id}
-                onClick={() => onUserSelect(user)}
-                selected={selectedUser?.id === user.id}
-                sx={{
-                  bgcolor: selectedUser?.id === user.id ? "rgba(25, 118, 210, 0.08)" : "transparent",
-                }}
-              >
-                <ListItemAvatar>
-                  <Avatar sx={{ bgcolor: selectedUser?.id === user.id ? "primary.main" : "grey.400" }}>
-                    {user.avatar}
-                  </Avatar>
-                </ListItemAvatar>
-                <ListItemText
-                  primary={user.name}
-                  secondary={
-                    <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-                      <Typography variant="body2" component="span">
-                        {user.phone}
-                      </Typography>
-                      <Typography variant="caption" component="span" sx={{ ml: 1, opacity: 0.7 }}>
-                        {user.lastTransaction}
-                      </Typography>
-                    </Box>
-                  }
-                />
-              </RecentUserItem>
-            ))}
-          </List>
-        </Box>
-        <Dialog open={dialog.open} onClose={onCloseDialog}>
-          <DialogTitle>Thông báo</DialogTitle>
-          <DialogContent>
-            <Typography>{dialog.message}</Typography>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={onCloseDialog} color="primary">
-              Đóng
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </CardContent>
-    </StyledCard>
-  )
-}
+//           <List sx={{ py: 0 }}>
+//             {recentUsers.map((user) => (
+//               <RecentUserItem
+//                 key={user.id}
+//                 onClick={() => onUserSelect(user)}
+//                 selected={selectedUser?.id === user.id}
+//                 sx={{
+//                   bgcolor: selectedUser?.id === user.id ? "rgba(25, 118, 210, 0.08)" : "transparent",
+//                 }}
+//               >
+//                 <ListItemAvatar>
+//                   <Avatar sx={{ bgcolor: selectedUser?.id === user.id ? "primary.main" : "grey.400" }}>
+//                     {user.avatar}
+//                   </Avatar>
+//                 </ListItemAvatar>
+//                 <ListItemText
+//                   primary={user.name}
+//                   secondary={
+//                     <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+//                       <Typography variant="body2" component="span">
+//                         {user.phone}
+//                       </Typography>
+//                       <Typography variant="caption" component="span" sx={{ ml: 1, opacity: 0.7 }}>
+//                         {user.lastTransaction}
+//                       </Typography>
+//                     </Box>
+//                   }
+//                 />
+//               </RecentUserItem>
+//             ))}
+//           </List>
+//         </Box>
+//         <Dialog open={dialog.open} onClose={onCloseDialog}>
+//           <DialogTitle>Thông báo</DialogTitle>
+//           <DialogContent>
+//             <Typography>{dialog.message}</Typography>
+//           </DialogContent>
+//           <DialogActions>
+//             <Button onClick={onCloseDialog} color="primary">
+//               Đóng
+//             </Button>
+//           </DialogActions>
+//         </Dialog>
+//       </CardContent>
+//     </StyledCard>
+//   )
+// }
